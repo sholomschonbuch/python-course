377e25395859651244da5fea12d6ca24b1f53f49
Sholom <sholomschonbuch@gmail.com>
2022-04-11 00:27:40 -0400

    made my hangman game funcional without graphics yet

--- 6th day. hangman/hangman.py
+++ 6th day. hangman/hangman.py
@@ -1,4 +1,3 @@
-#Step 1 
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -6,14 +5,30 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-while 
-guess = input("guess a letter: \n").lower()
-print(f"answer is {chosen_word}")
 answer_list = []
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for letter in chosen_word:
-    if guess == letter:
-        answer_list.append(guess)
-print(answer_list)
+    answer_list.append("_")
+game_counter = 6
+game = ""
+while game_counter > 0 and game != "won":
+
+    guess = input("guess a letter: \n").lower()
+    print(f"answer is {chosen_word}")
+
+    #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    counts = 0
+    for letter in chosen_word:
+        if guess == letter:
+            answer_list[counts] = guess
+        counts += 1
+    if answer_list.count("_") == 0:
+        game = "won"
+    game_counter -= 1
+            
+            
+    print(answer_list)
+if game == "won":
+    print("you won!")
+else:
+    print("you lost!")
 
