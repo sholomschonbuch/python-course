d283f4c3cfec9da18c04ad090ddec0772a288d67
Sholom <sholomschonbuch@gmail.com>
2022-04-12 00:46:02 -0400

    working on the graphics and a few bugs in hangman

---
+++ 6th day. hangman/clue.py
@@ -0,0 +1,8 @@
+
+
+a = 1
+b = a
+print(a + b)
+a = 5
+if a == b:
+    print("yes")
--- 6th day. hangman/hangman.py
+++ 6th day. hangman/hangman.py
@@ -4,31 +4,95 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
+#asq art
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 answer_list = []
 for letter in chosen_word:
     answer_list.append("_")
 game_counter = 6
-game = ""
-while game_counter > 0 and game != "won":
+game = False
+while game_counter > 0 and game == False:
 
     guess = input("guess a letter: \n").lower()
     print(f"answer is {chosen_word}")
 
     #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    
+    second = answer_list
     counts = 0
     for letter in chosen_word:
         if guess == letter:
             answer_list[counts] = guess
         counts += 1
-    if answer_list.count("_") == 0:
-        game = "won"
-    game_counter -= 1
+    if second == answer_list:
+        game_counter -= 1
+
+    if not "_" in answer_list:
+        game = True
+    print(stages[game_counter])
+    
             
             
     print(answer_list)
-if game == "won":
+if game == True:
     print("you won!")
 else:
+    print(stages[0])
     print("you lost!")
 
