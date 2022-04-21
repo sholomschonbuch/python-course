c3e79554f3ac11808165f15cee7b7a46ee3d341d
Sholom <sholomschonbuch@gmail.com>
2022-04-20 23:47:26 -0400

    Finished the hang man game

--- 6th day. hangman/clue.py
+++ 6th day. hangman/clue.py
@@ -1,8 +1,12 @@
 
 
-a = 1
-b = a
-print(a + b)
-a = 5
-if a == b:
-    print("yes")
+#a = 1
+#b = a
+#print(a + b)
+#a = 5
+#if a == b:
+#    print("yes")
+list = ["python", "http", "help"]
+for name in enumerate(list):
+    
+    print(name[0])
--- 6th day. hangman/hangman.py
+++ 6th day. hangman/hangman.py
@@ -1,98 +1,49 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
+from word_list import word_list
+from hangman_art import stages, logo
+import os
+clear = lambda: os.system('cls')
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-#asq art
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 answer_list = []
 for letter in chosen_word:
     answer_list.append("_")
-game_counter = 6
+lives = 6
 game = False
-while game_counter > 0 and game == False:
+while lives > 0 and game == False:
 
     guess = input("guess a letter: \n").lower()
     print(f"answer is {chosen_word}")
+    clear()
 
     #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-    
-    second = answer_list
-    counts = 0
-    for letter in chosen_word:
-        if guess == letter:
-            answer_list[counts] = guess
-        counts += 1
-    if second == answer_list:
-        game_counter -= 1
+    if guess in answer_list:
+        print("You already guessed it")
+    correct = False
+    for letter in enumerate(chosen_word):
+        if guess == letter[1]:
+            answer_list[letter[0]] = guess
+            correct = True
 
-    if not "_" in answer_list:
-        game = True
-    print(stages[game_counter])
-    
-            
-            
+    if correct == False:
+        lives -= 1
+    print(stages[lives])
+    print(f"you guessed {guess}")
     print(answer_list)
-if game == True:
-    print("you won!")
-else:
-    print(stages[0])
-    print("you lost!")
+    if not "_" in answer_list:
+        print("congrats you won!")
+        game = True
+    
+    if lives == 0:
+        print(stages[0])
+        print("you lost!")
+                
+print(logo)            
+
+
+
 
