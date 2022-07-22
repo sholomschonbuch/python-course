7e674c74dbd14fd4562781c1b1d0921127e09e2e
Sholom <sholomschonbuch@gmail.com>
2022-07-22 16:45:44 -0400

    started a secret betting using dictionaries

---
+++ 8th day. dictionary/secret_bid.py
@@ -0,0 +1,30 @@
+
+contin = "yes"
+biders = {}
+highest_bid = 0
+while contin == "yes":
+        
+    name = input("What's your name? ")
+    bid = int(input("What's your bid? $"))
+    biders[name] = bid
+    contin = input("Are there more bidders? type yes or no")
+
+for bid in biders:
+    if biders[bid] > highest_bid:
+        highest_bid = biders[bid]
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+print(biders[name])
+print(f"The highest bid was {highest_bid}")
+
