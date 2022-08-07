f60f95930cacf5f61220d33924f97f90697d1569
Sholom <sholomschonbuch@gmail.com>
2022-08-07 16:47:50 +0300

    Finally finished Secret Bid project, Made it fully optimal

--- 8th day. dictionary/secret_bid.py
+++ 8th day. dictionary/secret_bid.py
@@ -1,35 +1,34 @@
 import os
+from art1 import logo
 clear = lambda: os.system('cls')
 
-contin = "yes"
+#variables
+contin = False
+
+#dictionary of all bidders
 biders = {}
-highest_bid = 0
-while contin == "yes":
+
+#functions
+def find_highest_bidder(**dictionary1):
+    highest_bid = 0
+    for k,bid in dictionary1.items():
+        if bid > highest_bid:
+            highest_bid = bid
+            top = f"{k} at {bid}"
+    print(logo)
+    print(f"The highest bid was {top}")
+
+
+#While loop that will run the program
+while contin == False:
         
     name = input("What's your name? ")
     bid = int(input("What's your bid? $"))
     biders[name] = bid
-    contin = input("Are there more bidders? type yes or no. ")
+    quest = input("Are there more bidders? type yes or no. ")
+    if quest == "no":
+        contin = True
     clear()
 
-for k,bid in biders.items():
-    if bid > highest_bid:
-        highest_bid = bid
-        top = f"{k} at {bid}"
-logo = '''
-                         ___________
-                         \         /
-                          )_______(
-                          |"""""""|_.-._,.---------.,_.-._
-                          |       | | |               | | ''-.
-                          |       |_| |_             _| |_..-'
-                          |_______| '-' `'---------'` '-'
-                          )"""""""(
-                         /_________\\
-                       .-------------.
-                      /_______________\\
-'''
-print(f"The highest bid was {top}")
-print(logo)
-
-
+#Result
+find_highest_bidder(**biders)
